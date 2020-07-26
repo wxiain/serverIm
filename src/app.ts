c5071e94c21686763e80697dff40845fde98d82e
wxia <qlhdtly12189@163.com>
2020-07-26 23:55:55 +0800

    format

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 const app = express();
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello word');
-})
+});
 app.listen(process.env.PORT, () => {
   console.log(process.env.PORT + '服务启动成功');
-})
+});
