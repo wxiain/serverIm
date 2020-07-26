074d84d366698d34359f68e460906bb7047e7b7e
wxia <qlhdtly12189@163.com>
2020-07-26 23:38:03 +0800

    路由挂载和路由参数解析

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import bodyParser from 'body-parser';
+import router from './router';
+
 const app = express();
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello word');
-});
+// parse application/x-www-form-urlencoded, extended: false表示为字符串和数组, true表示任意类型
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(router);
+
 app.listen(process.env.PORT, () => {
   console.log(process.env.PORT + '服务启动成功');
 });
