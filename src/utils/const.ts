e2b9c76818b21d03562bd5c687f61058d5b8674c
wxia <qlhdtly12189@163.com>
2020-07-27 23:09:40 +0800

    公共方法

---
+++ src/utils/const.ts
@@ -0,0 +1,12 @@
+import { ReturnOperatorData, ReturnObjectData, ReturnPageData } from '../types/returnDataConstruction';
+export function operatorMessage({ res, message, status }: ReturnOperatorData) {
+  res.json({ message, status });
+}
+
+export function returnObject({ res, message, data, status }: ReturnObjectData) {
+  res.json({ message, data, status });
+}
+
+export function returnPageList<T>({ res, data, page, per_page }: ReturnPageData<T>) {
+  res.json({ data, page, per_page });
+}
