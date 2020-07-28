e92df6784d3bf0d2fc2e25de0237f083d5981225
wxia <qlhdtly12189@163.com>
2020-07-28 22:12:49 +0800

    测试

--- src/models/users/index.ts
+++ src/models/users/index.ts
@@ -1,11 +1,15 @@
 import { MiddlewareParams } from '../../types/express.extends';
 import { operatorMessage, returnObject, returnPageList } from '../../utils/const';
 const fn: MiddlewareParams = function (req, res) {
-  returnPageList<{}>({
+  /*returnPageList<{}>({
     res,
     data: [{}],
     page: 1,
-    per_page: 10
-  });
+    per_page: 10,
+    total: 1,
+    statusCode: 200
+  });*/
+  // operatorMessage({ res, message: '2222', status: true,  statusCode: 200 });
+  returnObject({ res, message: '333', data: {}, status: true, statusCode: 200 });
 };
 export default fn;
