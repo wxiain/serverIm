0f55ff5ff0519223cf4d97a185e0e9da9e333113
wxia <qlhdtly12189@163.com>
2020-07-29 22:59:03 +0800

    添加 auth/user 接口

--- src/router/users/index.ts
+++ src/router/users/index.ts
@@ -1,6 +1,10 @@
 import router from '../createRouter';
 import Home from '../../models/users';
 import UserLogin from '../../models/users/login';
+import getUserInfo from '../../models/users/getUserInfo';
+
+import check from '../../middlewares/jwt/check';
 
 router.get('/', Home);
 router.post('/login', UserLogin);
+router.get('/auth/user', check, getUserInfo);
