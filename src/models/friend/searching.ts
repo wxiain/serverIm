9aca3004e8d8c6e087c78ffc78a9a1ff01b7b73f
wxia <qlhdtly12189@163.com>
2020-08-05 23:01:11 +0800

    分页信息修改

--- src/models/friend/searching.ts
+++ src/models/friend/searching.ts
@@ -3,15 +3,19 @@ import { returnObject, returnPageList } from '../../utils/const';
 import db from '../../database/db';
 
 const searching: MiddlewareParams = function (req, res) {
-  let keywords = req.query;
+  let query = req.query;
+  let keywords = query.keywords;
+  let page = Number(query.page || '0');
+  let page_size = Number(query.page_size || '10');
   if (!keywords) {
     returnObject({ res, statusCode: 400, status: false, message: '请输入关键字以检索', data: [] });
     return;
   }
   let sql = `SELECT * FROM users WHERE id<>${req.userId} AND (name LIKE '%${keywords}%' OR nickname LIKE '%${keywords}%' OR mobile LIKE '%${keywords}%')`;
+  page += 1;
   db(sql).then((result: any) => {
     res.status(200);
-    returnPageList({ res, statusCode: 200, data: result, page: 1, per_page: 10, total: 1 });
+    returnPageList({ res, statusCode: 200, data: result, page, page_size, total: 1 });
   });
 };
 
