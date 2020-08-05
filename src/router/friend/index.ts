ee59397a0b4c18767b2aa78b22e55205fb08b8d1
wxia <qlhdtly12189@163.com>
2020-08-05 23:02:02 +0800

    朋友列表的路由添加

--- src/router/friend/index.ts
+++ src/router/friend/index.ts
@@ -1,5 +1,7 @@
 import router from '../createRouter';
 import check from '../../middlewares/jwt/check';
 import searching from '../../models/friend/searching';
+import myList from '../../models/friend/myList';
 
 router.get('/friend/searching', check, searching);
+router.get('/friend/myList', check, myList);
