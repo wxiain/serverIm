57f2fe0ecf0e17f9c1807e604253d2f5396f2570
wxia <qlhdtly12189@163.com>
2020-08-10 00:26:55 +0800

    在公共返回值方法中写入http状态码

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,12 +1,25 @@
-import { ReturnOperatorData, ReturnObjectData, ReturnPageData } from '../types/returnDataConstruction';
+import {
+  ReturnOperatorData,
+  ReturnObjectData,
+  ReturnPageData,
+  ReturnErrorMessage
+} from '../types/returnDataConstruction';
 export function operatorMessage({ res, message, status, statusCode }: ReturnOperatorData) {
+  res.status(statusCode);
   res.json({ message, status, statusCode });
 }
 
 export function returnObject({ res, message, data, status, statusCode, token }: ReturnObjectData) {
+  res.status(statusCode);
   res.json({ message, data, status, statusCode, token });
 }
 
 export function returnPageList<T>({ res, data, page, page_size, total, statusCode }: ReturnPageData<T>) {
+  res.status(statusCode);
   res.json({ data, page, page_size, total, statusCode });
+}
+
+export function returnErrorMessage({ res, data, statusCode }: ReturnErrorMessage) {
+  res.status(statusCode);
+  res.json(data);
 }
