1c138f14ce6764111db6f315ffe4cb1e7b5e4847
wxia <qlhdtly12189@163.com>
2020-08-10 23:02:48 +0800

    interface Apply

--- src/types/users.ts
+++ src/types/users.ts
@@ -2,3 +2,14 @@ export interface UsersLogin {
   username: string;
   password: string;
 }
+
+export interface Apply {
+  name: string;
+  nickname: string;
+  avatar: string;
+  age: number;
+  gender: string;
+  mobile: string;
+  message: string;
+  apply_id: number;
+}
