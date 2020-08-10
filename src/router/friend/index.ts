e426ab72e41f35abe2bebc348f9c0838e7bbad99
wxia <qlhdtly12189@163.com>
2020-08-10 23:02:23 +0800

    申请好友接口

--- src/router/friend/index.ts
+++ src/router/friend/index.ts
@@ -3,8 +3,10 @@ import routes from '../createRoutes';
 import check from '../../middlewares/jwt/check';
 import searching from '../../models/friend/searching';
 import myList from '../../models/friend/myList';
+import apply from '../../models/friend/apply';
 
 routes.get('/searching', check, searching);
 routes.get('/myList', check, myList);
+routes.post('/apply', check, apply);
 
 router.use('/friend', routes);
