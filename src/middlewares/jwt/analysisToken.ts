33c77203f321b0552df19bf65e4ac567489396aa
wxia <qlhdtly12189@163.com>
2020-08-14 22:59:53 +0800

    解析token 独立成一个方法, 供全局使用

---
+++ src/middlewares/jwt/analysisToken.ts
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken';
+// 函数重载, 设置process
+declare let process: {
+  env: {
+    TOKEN_KEY: string;
+    TOKEN_EXPIRES_IN: string;
+  };
+};
+interface AnalysisToken {
+  statusCode: number;
+  message?: string;
+  detail?: string;
+  userId?: number;
+}
+
+export default function (token: string): AnalysisToken {
+  try {
+    let decoded: any = jwt.verify(token, process.env.TOKEN_KEY);
+    let date: number = new Date().getTime();
+    if (decoded.exp <= date / 1000) {
+      return {
+        statusCode: 200,
+        userId: decoded.userId
+      };
+    } else {
+      return {
+        statusCode: 401,
+        message: '用户身份过期',
+        detail: 'Unauthenticated.'
+      };
+    }
+  } catch (e) {
+    return {
+      statusCode: 401,
+      message: '用户身份过期',
+      detail: 'Unauthenticated.'
+    };
+  }
+}
