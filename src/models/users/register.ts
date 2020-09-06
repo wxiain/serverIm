e9bdffb8224aff5bd9572fd378de15b8247b3404
wxia <qlhdtly12189@163.com>
2020-09-06 23:20:11 +0800

    register 查询是否有相同的用户名时优化

--- src/models/users/register.ts
+++ src/models/users/register.ts
@@ -1,15 +1,15 @@
 import db from '../../database/db';
-import {MiddlewareParams} from '../../types/express.extends';
-import {returnErrorMessage, operatorMessage, returnObject} from '../../utils/const';
+import { MiddlewareParams } from '../../types/express.extends';
+import { returnErrorMessage, operatorMessage, returnObject } from '../../utils/const';
 import create from '../../middlewares/jwt/create';
-import {UsersRegister} from '../../types/users';
+import { UsersRegister } from '../../types/users';
 
-const register: MiddlewareParams = function(req, res) {
-  let params:UsersRegister = req.body;
+const register: MiddlewareParams = function (req, res) {
+  let params: UsersRegister = req.body;
   let validate = [
     { value: params.password, key: 'password' },
     { value: params.username, key: 'username' },
-    { value: params.confirmPassword, key: 'confirmPassword' },
+    { value: params.confirmPassword, key: 'confirmPassword' }
   ];
   for (let item of validate) {
     if (!item.value) {
@@ -18,28 +18,31 @@ const register: MiddlewareParams = function(req, res) {
     }
   }
   if (params.confirmPassword !== params.password) {
-    return operatorMessage({res, statusCode: 400, message:'密码和确认密码不一致', status: false})
+    return operatorMessage({ res, statusCode: 400, message: '密码和确认密码不一致', status: false });
   }
-  db(`SELECT id FROM users WHERE username=${params.username}`).then((result:any) => {
-    if (result.length) {
-      return operatorMessage({res, status: false, message: '该用户名已被使用', statusCode: 400});
-    }
-    return db(`INSERT INTO users (username,password) VALUES (${params.username},${params.password});`)
-  }).then((result: any) => {
-    // select @@IDENTITY as id;
-    let id:number = result.insertId;
-    let object = {
-      res,
-      message: '注册成功',
-      status: true,
-      statusCode: 200,
-      data: {
-        username: params.username,
-        id
-      },
-    }
-    returnObject(Object.assign({}, object, params.type === 'login'? {token: create({id})}: {}));
-  })
-}
+  db(`INSERT INTO users (username,password) VALUES (${params.username},${params.password});`)
+    .then((result: any) => {
+      // select @@IDENTITY as id;
+      let id: number = result.insertId;
+      let object = {
+        res,
+        message: '注册成功',
+        status: true,
+        statusCode: 200,
+        data: {
+          username: params.username,
+          id
+        }
+      };
+      returnObject(Object.assign({}, object, params.type === 'login' ? { token: create({ id }) } : {}));
+    })
+    .catch((err: { errno: number }) => {
+      if (err.errno === 1062) {
+        operatorMessage({ res, status: false, message: '该用户名已被使用', statusCode: 400 });
+      } else {
+        returnErrorMessage({ res, statusCode: 500, data: err });
+      }
+    });
+};
 
 export default register;
