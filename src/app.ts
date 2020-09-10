6944ec7864362c61533550af33dc9fd88c75d3e3
wxia <qlhdtly12189@163.com>
2020-09-10 23:59:23 +0800

    cors

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,17 @@ import { Server } from 'http';
 import Socket from './models/sokcet';
 
 const app = express();
+// app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Authorization,X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, PUT, DELETE');
+  res.header('Allow', 'GET, POST, PATCH, OPTIONS, PUT, DELETE');
+  next();
+});
 // parse application/x-www-form-urlencoded, extended: false表示为字符串和数组, true表示任意类型
 app.use(bodyParser.urlencoded({ extended: true }));
 
