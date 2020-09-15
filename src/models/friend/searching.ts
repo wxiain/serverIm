b3369c8fad9716dfbd4838a335a5ac5069bc89d8
wxia <qlhdtly12189@163.com>
2020-09-15 09:13:37 +0800

    检索  sql语句分页和去除name字段

--- src/models/friend/searching.ts
+++ src/models/friend/searching.ts
@@ -11,11 +11,10 @@ const searching: MiddlewareParams = function (req, res) {
     returnObject({ res, statusCode: 400, status: false, message: '请输入关键字以检索', data: [] });
     return;
   }
-  let sql = `SELECT * FROM users WHERE id<>${req.userId} AND (name LIKE '%${keywords}%' OR nickname LIKE '%${keywords}%' OR mobile LIKE '%${keywords}%')`;
-  page += 1;
-  db(sql)
+  let sql = `SELECT * FROM users WHERE id<>${req.userId} AND (useranme LIKE '%${keywords}%' OR mobile LIKE '%${keywords}%') ORDER BY id LIMIT ${page},${page_size}`;
+  db(sql, true)
     .then((result: any) => {
-      returnPageList({ res, statusCode: 200, data: result, page, page_size, total: 1 });
+      returnPageList({ res, statusCode: 200, total: result.total, data: result.data, page: page + 1, page_size });
     })
     .catch((err) => {
       returnErrorMessage({ res, data: err, statusCode: 500 });
