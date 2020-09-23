ab3a2fcc9e1f0e9d1ff1b6dd22c58facef3ea0fc
wxia <qlhdtly12189@163.com>
2020-09-23 18:01:00 +0800

    searching sql fix

--- src/models/friend/searching.ts
+++ src/models/friend/searching.ts
@@ -2,7 +2,7 @@ import { MiddlewareParams } from '../../types/express.extends';
 import { returnErrorMessage, returnObject, returnPageList } from '../../utils/const';
 import db from '../../database/db';
 
-const searching: MiddlewareParams = function (req, res) {
+const searching: MiddlewareParams = function(req, res) {
   let query = req.query;
   let keywords = query.keywords;
   let page = Number(query.page || '0');
@@ -11,10 +11,10 @@ const searching: MiddlewareParams = function (req, res) {
     returnObject({ res, statusCode: 400, status: false, message: '请输入关键字以检索', data: [] });
     return;
   }
-  let sql = `SELECT DISTINCT users.id,users.username,users.nickname,users.mobile,users.gender,users.age,users.avatar,users.address
+  let sql = `SELECT users.id,users.username,users.nickname,users.mobile,users.gender,users.age,users.avatar,users.address
     FROM users LEFT JOIN friends
-    ON users.id<>${req.userId} 
-    AND users.id <> friends.relation_id
+    ON users.id <> friends.relation_id
+    WHERE users.id<>${req.userId}
     AND (instr(users.username, '${keywords}') > 0 OR instr(users.mobile, '${keywords}') > 0)
     ORDER BY users.id LIMIT ${Math.max(0, page - 1) * page_size},${page_size}`;
   db(sql, true)
