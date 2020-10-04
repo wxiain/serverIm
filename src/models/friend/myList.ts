aea89c82f7eb5844d297808447b17d55a6fcff98
wixa <qlhdtly12189@163.com>
2020-10-04 23:35:54 +0800

    朋友列表修改

--- src/models/friend/myList.ts
+++ src/models/friend/myList.ts
@@ -1,22 +1,30 @@
 import { MiddlewareParams } from '../../types/express.extends';
 import db from '../../database/db';
 import { returnPageList, returnErrorMessage } from '../../utils/const';
+import { pagination } from '../../utils/method';
 
 declare let process: {
   env: {
     PAGE_SIZE: string;
+    PAGE: string;
   };
 };
 
 const myList: MiddlewareParams = function (req, res) {
   let query = req.query;
-  let page = Number(query.page || '1') - 1;
+  let page = Number(query.page || process.env.PAGE);
   let page_size = Number(query.page_size || process.env.PAGE_SIZE);
-  let sql = `SELECT * FROM friends WHERE user_id = ${req.userId} ORDER BY id LIMIT ${page},${page_size}`;
-  page = page + 1;
+  let sql = `SELECT * FROM friends 
+        WHERE apply_id = ${req.userId} OR relation_id=${req.userId} 
+        ORDER BY id LIMIT ${pagination(page, page_size)}`;
   db(sql, true)
     .then((result: any) => {
-      returnPageList({ res, page, page_size, data: result.data, total: result.total, statusCode: 200 });
+      let data = result.data;
+      for (let item of data) {
+        item.user = JSON.parse(item.user);
+        item.apply = JSON.parse(item.apply);
+      }
+      returnPageList({ res, page, page_size, data, total: result.total, statusCode: 200 });
     })
     .catch((err) => {
       returnErrorMessage({ res, statusCode: 500, data: err });
