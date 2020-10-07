5b7bfefa419109b9c879bec7897682414e1c3b42
wxia <qlhdtly12189@163.com>
2020-10-07 21:22:47 +0800

    links接口修改

--- src/models/message/links.ts
+++ src/models/message/links.ts
@@ -1,6 +1,7 @@
 import { MiddlewareParams } from '../../types/express.extends';
 import db from '../../database/db';
-import { returnPageList, returnErrorMessage } from '../../utils/const';
+import { returnPageList, returnErrorMessage, operatorMessage } from '../../utils/const';
+import { pagination, sortReturnString } from '../../utils/method';
 
 declare let process: {
   env: {
@@ -13,10 +14,20 @@ const links: MiddlewareParams = function (req, res) {
   let query = req.query;
   let page = Number(query.page || process.env.PAGE);
   let page_size = Number(query.page_size || process.env.PAGE_SIZE);
-  let sql = `SELECT * FROM links WHERE userId=${req.userId} OR sendId=${req.userId} ORDER BY id LIMIT ${page},${page_size}`;
-  db(sql)
+  if (!query.id) {
+    operatorMessage({ res, statusCode: 400, status: false, message: '对方的id是必须的' });
+    return;
+  }
+  let sql = `SELECT * FROM links 
+    WHERE ids=${sortReturnString(Number(req.userId), Number(query.id))} 
+    ORDER BY id LIMIT${pagination(page, page_size)}`;
+  db(sql, true)
     .then((result: any) => {
-      returnPageList({ res, statusCode: 200, page: page + 1, page_size, data: result, total: 0 });
+      for (let item of result.data) {
+        item.user = JSON.parse(item.user);
+        item.receive = JSON.parse(item.receive);
+      }
+      returnPageList({ res, statusCode: 200, page, page_size, data: result.data, total: result.total });
     })
     .catch((err) => {
       returnErrorMessage({ res, statusCode: 500, data: err });
