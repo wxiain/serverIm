c3241eb2a8c982ca52be49dea0e22ef303c8996b
wxia <qlhdtly12189@163.com>
2020-10-07 21:23:33 +0800

    消息列表接口 sql修改

--- src/models/message/messages.ts
+++ src/models/message/messages.ts
@@ -13,7 +13,9 @@ const messages: MiddlewareParams = function (req, res) {
   let query = req.query;
   let page = Number(query.page || process.env.PAGE);
   let page_size = Number(query.page_size || process.env.PAGE_SIZE);
-  let sql = `SELECT * FROM messages WHERE userId=${req.userId} ORDER BY id LIMIT ${page},${page_size}`;
+  let sql = `SELECT * FROM messages 
+        WHERE (receive_id=${req.userId} AND send_id=${query.id}) OR (receive_id=${query.id} AND send_id=${req.userId}) 
+        ORDER BY id LIMIT ${page},${page_size}`;
   db(sql, true)
     .then((result: any) => {
       returnPageList({ res, statusCode: 200, total: result.total, data: result.data, page_size, page: page + 1 });
